853a2262e68f60b7a97846f9a7124de8c82355fc
vanyadeg <id1997@yandex.ru>
2018-03-30 20:32:46 +0300

    indexes

--- indexes/indexes/index.py
+++ indexes/indexes/index.py
@@ -1,37 +1,83 @@
 from collections import defaultdict
+import pickle as pkl
+import struct
 
 from indexes.docwork import docreader
 from indexes.docwork.doc2words import extract_words
 
-mdict = defaultdict(lambda : [])
-urls = []
-
 
 def code_to_byte(id):
-    res = []
-    # todo
+    result = bytearray()
+    while id > 127:
+        result.append(id & 127)
+        id >>= 7
+    result.append((id & 127) + 128)
+    return result
 
 
-def varbyte(docids):
-    for i in range(1, len(docids)):
-        docids[i] = docids[i] - docids[i - 1]
-    docids.remove(0)
+def decode_vb(coded_id):
+    x = 0
+    for i, code in enumerate(coded_id):
+        if code < 128:
+            x += (1 << 7 * i) * code
+        elif code <= 255:
+            x += (1 << 7 * i) * (code - 128)
+            break
+    return x
 
-    res = []
+
+def decode_vb_array(vb):
+    coded_ids = []
+    prev = -1
+    for i, s in enumerate(vb):
+        if s > 127 <= 255:
+            coded_ids.append(vb[(prev + 1):(i + 1)])
+            prev = i
+    diffs = [decode_vb(coded_id) for coded_id in coded_ids]
+    res = [diffs[0]]
+    for diff in diffs[1:]:
+        res.append(res[-1] + diff)
+    return res
+
+
+def code_varbyte(docids):
+    res = bytearray()
     for id in docids:
         res += code_to_byte(id)
-
+    res = struct.pack('I',  len(res)) + res
     return res
 
 
 if __name__ == '__main__':
     reader = docreader.DocumentStreamReader(docreader.parse_command_line().files)
+    mdict = defaultdict(lambda: [])
+    urls = []
 
     for doc in reader:
         urls.append(doc.url)
         for word in extract_words(doc.text):
             mdict[word].append(len(urls))
+    for term in mdict.keys():
+        docids = mdict[term]
+        for i in reversed(range(1, len(docids))):
+            docids[i] = docids[i] - docids[i - 1]
+        docids = filter(lambda x: x != 0, docids)
+        mdict[term] = docids
+
+    print mdict['mirror']
 
     for word in mdict.keys():
-        mdict[word] = varbyte(mdict[word])
-    # todo
+        mdict[word] = code_varbyte(mdict[word])
+
+    id_url = {}
+    term_position = {}
+    with open('index', 'wb') as f:
+        for term, coded_ids in mdict.iteritems():
+            term_position[term] = f.tell()
+            f.write(coded_ids)
+    with open('term_position', 'wb') as f:
+        pkl.dump(term_position, f)
+    with open('id_url', 'wb') as f:
+        for i, url in enumerate(urls):
+            id_url[(i + 1)] = url
+        pkl.dump(id_url, f)
---
+++ indexes/indexes/search.py
@@ -0,0 +1,45 @@
+import pickle as pkl
+import struct
+
+from indexes.index import decode_vb_array
+
+id_url = {}
+term_position = {}
+
+
+def find_docs(term, index):
+    pos = term_position.get(term, None)
+    if pos is None:
+        return set()
+    else:
+        index.seek(pos)
+        vb_len = struct.unpack('I', index.read(4))[0]
+        vb = map(ord, index.read(vb_len))
+        return set(decode_vb_array(vb))
+
+
+def search(terms, index):
+    docs = find_docs(terms[0], index)
+    for term in terms[1:]:
+        docs.intersection(find_docs(term, index))
+    return docs
+
+
+if __name__ == '__main__':
+    with open('id_url', 'rb') as f:
+        id_url = pkl.load(f)
+    with open('term_position', 'rb') as f:
+        term_position = pkl.load(f)
+
+    with open('index', 'rb') as index:
+        while True:
+            try:
+                query = raw_input()
+                print query
+                query = query.decode("utf-8")
+                docs = search(query.strip().lower().replace(' ', '').split('&'), index)
+                print len(docs)
+                for id in docs:
+                    print id_url[id]
+            except EOFError:
+                break
